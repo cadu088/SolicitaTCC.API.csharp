0d2c4ddb94ec25face46b017dae569a18a7cd378
Carlos Rodrigues <carloseduardors088@gmail.com>
2023-05-13 18:05:52 -0300

    Tentativa publicação

--- SolicitaTCC.API.csharp/Program.cs
+++ SolicitaTCC.API.csharp/Program.cs
@@ -29,11 +29,11 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseCors("MyPolicy");
 
